f407f9f367507676028bc4c14f00636e3112e8eb
Katherine Mattison <kbmedisto@gmail.com>
2014-06-03 11:09:07 -0400

    first commit for nft chas

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('nftv1App', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
